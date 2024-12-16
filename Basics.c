48c3b2e175f01355b965eef5a040f0f31a43181a
HimanshuHeda <harsh967896@gmail.com>
2024-12-16 09:46:34 +0530

    basics

---
+++ Basics.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int a,b;
+    printf("Enter the First Value : ");
+    scanf("%d",&a);
+    printf("Enter the 2nd Value : ");
+    scanf("%d",&b);
+    printf("First number : %d\n", a);
+    printf("2nd number : %d\n", b);
+    printf("Sum of 2 numbers are : %d\n", a+b);
+}
